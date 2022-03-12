678c1c7f63d9114b9795c44af1dba451bbe888a0
Miftari Simel <miftarisimel@gmail.com>
2022-03-12 21:25:55 +0100

    Should make portfolio single item

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -6,6 +6,32 @@ function Contact() {
   return (
       <div className="Contact">
         <Heading title={"Contact Me"} />
+        <div className={"email-contact container bg-secondary w-75"}>
+          <h1 className={"section fw-bold text-center py-5 text-light"}>Contact me via Email</h1>
+          <div className="row justify-content-center pb-5">
+            <div className="col-lg-4">
+              <input className="form-control m-3" type="text" placeholder="Name" />
+              <input className="form-control m-3" type="text" placeholder="Email" />
+              <input className="form-control m-3" type="text" placeholder="Subject" />
+            </div>
+            <div className="col-lg-4 text-center">
+              <textarea className="form-control m-3" rows="3" placeholder="Body">
+
+              </textarea>
+              <button className='btn btn-gradient-outline mt-1'>Send Message</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={"email-contact container bg-secondary w-75"}>
+          <h1 className={"section fw-bold text-center py-5 text-light"}>Other ways to conctact me</h1>
+          <div className={"row text-center pb-5"}>
+            <h1 className={"fw-bold text-white"}>Still Working on this.... sorry! :(</h1>
+          </div>
+
+        </div>
+
+
       </div>
   );
 }
