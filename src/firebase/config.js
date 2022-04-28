d79037d46663dd33aa02229ff89167c26bee922e
Miftari Simel <miftarisimel@gmail.com>
2022-04-28 18:57:01 +0200

    Firebase setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFPM6F3q2jJCdjVs3lF_g4SY-8EVCTYmE",
-  authDomain: "miftarisimel-5e19e.firebaseapp.com",
-  projectId: "miftarisimel-5e19e",
-  storageBucket: "miftarisimel-5e19e.appspot.com",
-  messagingSenderId: "378513130312",
-  appId: "1:378513130312:web:3abd854419088dd173a97d",
-  measurementId: "G-E2N1CHZ8RP"
+  apiKey: "AIzaSyC3ihAlwt6ftL9SVciFqHbpTafBH_iJ9Cs",
+  authDomain: "simelmiftari.firebaseapp.com",
+  projectId: "simelmiftari",
+  storageBucket: "simelmiftari.appspot.com",
+  messagingSenderId: "685022794858",
+  appId: "1:685022794858:web:c81e79956e948211e36613",
+  measurementId: "G-6LPKP3E09F"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export {app, analytics}
